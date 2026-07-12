a786d64de6a30ef7e9052ef18907da8290765cb8
kush9199 <kushaagr.sh@gmail.com>
2026-07-12 22:30:21 +0530

    initialized monorepo structure

---
+++ shared/javaLib/auth-filter/src/test/java/dev/monorepo/shared/auth_filter/AuthFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.monorepo.shared.auth_filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
